a747610113f58663ecd2129416fb974c23da379f
RicAlvesO <ricaoimp@gmail.com>
2022-02-09 14:10:38 +0000

    Added Chromium Browser Compability

---
+++ src/chromium/popup/events.js
@@ -0,0 +1,106 @@
+function isLive(name) {
+
+    //////////
+    // TODO //
+    //////////
+
+    return null;
+}
+
+//Create new stream user
+function addUser(name) {
+
+    if(!name){return;}
+    
+    //Live state
+    const state = document.createElement("div");
+    state.className = "state";
+    state.onmouseover = function () { state.textContent = "🗑️"; };
+    
+    //User name
+    const op = document.createElement("div");
+    op.className = "page-choice";
+    op.style.fontFamily = "Georgia, serif";
+    const node = document.createTextNode(name);
+    op.append(node);
+
+    //Row for new user
+    const row = document.createElement("div");
+    row.className = "row";
+    row.append(op);
+    row.append(state);
+    
+    //Check if live and add to list
+    const element = document.getElementById("channels");
+    const sta = isLive(name);
+    if (sta) {
+        const st = document.createTextNode("🟢");
+        state.onmouseleave = function () { state.textContent ="🟢"; };
+        state.append(st);
+        op.style.color = "#ffffffbb";
+        element.insertBefore(row, element.firstChild);
+    }
+    else if (sta === false) {
+        const st = document.createTextNode("🔴");
+        state.onmouseleave = function () { state.textContent ="🔴"; };
+        state.append(st);
+        op.style.color = "#00000077";
+        element.appendChild(row);
+    }
+    else {
+        const st = document.createTextNode("👀");
+        state.onmouseleave = function () { state.textContent ="👀"; };
+        state.append(st);
+        op.style.color = "#ffffffbb";
+        element.appendChild(row);
+    }
+}
+
+//Delete channels
+document.addEventListener("click", function (e) {
+    if (!e.target.classList.contains("state")) {
+        return;
+    }
+    chrome.storage.local.remove(e.target.parentNode.firstChild.firstChild.textContent);
+    e.target.parentNode.remove();
+});
+
+//Click on channels
+document.addEventListener("click", function (e) {
+    if (!e.target.classList.contains("page-choice")) {
+        return;
+    }
+
+    var chosenPage = "https://www.twitch.tv/" + e.target.textContent.replace(/(\r\n|\n|\r|\s)/gm, "");
+    chrome.tabs.create({
+        url: chosenPage
+    });
+
+});
+
+//Click on add new channel
+document.addEventListener("click", function (e) {
+    if (!e.target.classList.contains("commit")) {
+        return;
+    }
+
+    var f = document.getElementById("new_channel");
+    addUser(f.value);
+    chrome.storage.local.set({ [f.value]: 0 });
+    f.value = null;
+});
+
+function onError(error) {
+    console.log(error);
+}
+
+function initialize() {
+  chrome.storage.local.get(null,function(results) {
+    var sKeys = Object.keys(results);
+    for (let sKey of sKeys) {
+      addUser(sKey);
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', initialize);
